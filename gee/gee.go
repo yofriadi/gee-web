4cfefc80753ca69bdd932cfa7597c075c07b3e12
Yofriadi Yahya <yofriadiyahya@gmail.com>
2020-03-09 06:55:41 +0700

    MODIFY: cleanup unnecessary code

--- gee/gee.go
+++ gee/gee.go
@@ -9,37 +9,62 @@ import (
 type HandlerFunc func(*Context)
 
 // Engine implement the interface of ServeHTTP
-type Engine struct {
-	router *router
-}
+type (
+	RouterGroup struct {
+		prefix string
+		parent *RouterGroup // support nesting route
+		engine *Engine
+	}
+
+	Engine struct {
+		*RouterGroup
+		*router
+		groups []*RouterGroup
+	}
+)
 
 // New is the constructor of gee.Engine
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
-func (e *Engine) addRoute(method, pattern string, handler HandlerFunc) {
+// Group is defined to create new router group
+func (rg *RouterGroup) Group(prefix string) *RouterGroup {
+	newGroup := &RouterGroup{
+		prefix: rg.prefix + prefix,
+		parent: rg,
+		engine: rg.engine,
+	}
+	rg.engine.groups = append(rg.engine.groups, newGroup)
+	return newGroup
+}
+
+func (rg *RouterGroup) addRoute(method, comp string, handler HandlerFunc) {
+	pattern := rg.prefix + comp
 	log.Printf("Route %4s - %s", method, pattern)
-	e.router.addRoute(method, pattern, handler)
+	rg.engine.router.addRoute(method, pattern, handler)
 }
 
 // GET defines the method to add GET request
-func (e *Engine) GET(pattern string, handler HandlerFunc) {
-	e.addRoute("GET", pattern, handler)
+func (rg *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	rg.addRoute("GET", pattern, handler)
 }
 
 // POST defines the method to add POST request
-func (e *Engine) POST(pattern string, handler HandlerFunc) {
-	e.addRoute("POST", pattern, handler)
+func (rg *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	rg.addRoute("POST", pattern, handler)
+}
+
+// Run defines the method to start a http server
+func (e *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, e)
 }
 
 // ServeHTTP defines the method to serve http request
 func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	c := newContext(w, req)
 	e.router.handle(c)
-}
-
-// Run defines the method to start a http server
-func (e *Engine) Run(addr string) (err error) {
-	return http.ListenAndServe(addr, e)
 }
